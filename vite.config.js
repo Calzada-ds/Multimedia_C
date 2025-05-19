14a228422990581d8223e4c73b7cc0d465b5df50
Daniel Sebastian Calzada Guerrero <calzada-ds>
2025-05-19 00:03:33 -0600

    Se agrega proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: './',
+  base: './',
   plugins: [preact()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        titulacion: resolve(__dirname, 'titulacion.html'),
+        serviciosocial: resolve(__dirname, 'servicio-social.html'),
+        practicas: resolve(__dirname, 'practicas.html'),
+        documentacion: resolve(__dirname, 'documentacion.html'),
+      }
+    }
+  }
 })
